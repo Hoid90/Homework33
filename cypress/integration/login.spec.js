b5bea3aa3301f7bd566a94ff4bb4be62bc4d0722
Maxim Buryak <bouryakmaxim@gmail.com>
2021-11-18 14:55:52 +0200

    done

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,10 @@
+describe('User needs to', () => {
+    it('', () => {
+    cy.visit('https://cirosantilli-realworld-next.herokuapp.com/');
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get(':nth-child(1) > .form-control').type('mburyak@at.com');
+    cy.get(':nth-child(2) > .form-control').type('qwerty');
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'mburyak1')
+    })
+  })  
---
+++ cypress/integration/registration.spec.js
@@ -0,0 +1,19 @@
+describe('My Second Test', () => {
+let user;
+
+    before(function () {
+        cy.task("freshUser").then((freshUser) => {
+            user = freshUser;
+        });
+    });
+
+    it('Sign up with faker', () => {
+        cy.visit('https://cirosantilli-realworld-next.herokuapp.com/');
+        cy.get(':nth-child(3) > .nav-link').click();
+        cy.get(':nth-child(1) > .form-control').type(user.username);
+        cy.get(':nth-child(2) > .form-control').type(user.email);
+        cy.get(':nth-child(3) > .form-control').type(user.password);
+        cy.get('.btn').click();
+        cy.get(':nth-child(4) > .nav-link').should('contain', user.username);   
+      })
+    })
