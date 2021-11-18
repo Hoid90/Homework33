9559449f262a29ef5e6c8eb6ab17ea1f9b5faf64
Maxim Buryak <91210372+Hoid90@users.noreply.github.com>
2021-11-18 15:59:22 +0200

    Update registration.spec.js
    Line 17: added toLowerCase() to user.username

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -14,6 +14,6 @@ let user;
         cy.get(':nth-child(2) > .form-control').type(user.email);
         cy.get(':nth-child(3) > .form-control').type(user.password);
         cy.get('.btn').click();
-        cy.get(':nth-child(4) > .nav-link').should('contain', user.username);   
+        cy.get(':nth-child(4) > .nav-link').should('contain', user.username.toLowerCase());   
       })
     })
